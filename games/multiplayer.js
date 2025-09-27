950a38bc0d6570ce833b65b0bfd0ebd51ebbd40e
bluess1 <158479210+bluess1@users.noreply.github.com>
2025-09-26 21:44:09 -0400

    Update multiplayer.js

--- games/multiplayer.js
+++ games/multiplayer.js
@@ -1,5 +1,5 @@
 // WebSocket connection (add to multiplayer.js)
-const socket = io('your-server-url');
+const socket = io('bluess1.github.io/tmagames/');
 
 socket.on('playerJoined', (playerData) => {
     // Add remote player
