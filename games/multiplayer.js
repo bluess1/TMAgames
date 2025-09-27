3927f036a64d7c71db07395156fef530e49e2747
bluess1 <158479210+bluess1@users.noreply.github.com>
2025-09-26 21:43:04 -0400

    Update multiplayer.js

--- games/multiplayer.js
+++ games/multiplayer.js
@@ -1,1 +1,10 @@
+// WebSocket connection (add to multiplayer.js)
+const socket = io('your-server-url');
 
+socket.on('playerJoined', (playerData) => {
+    // Add remote player
+});
+
+socket.on('playerUpdate', (playerData) => {
+    // Update remote player position
+});
